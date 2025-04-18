92c908d9e62788d3a53ca0a666b92a1275f399c1
Dawn Connell <dawnthomsonconnell@gmail.com>
2025-04-18 17:45:35 +1000

    recipes: add yoghurt recipe

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -2,6 +2,7 @@ export default function Things() {
     return (
         <div>
             <h1>Recipes</h1>
+            <a href="/recipes/yoghurt">Yoghurt</a>
         </div>
     )
 }
---
+++ src/app/recipes/yoghurt/page.tsx
@@ -0,0 +1,31 @@
+export default function Yoghurt() {
+    return (
+        <div>
+            <h2>Yoghurt</h2>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>2tbsp plain, full fat yoghurt</li>
+                <li>2 litres full fat milk</li>
+            </ul>
+            <h3>Equipment</h3>
+                <li>Saucepan (roughly 3l capacity)</li>
+                <li>Wooden spoon</li>
+                <li>Heat source for saucepan (e.g. hob)</li>
+                <li>Two 2l glass jars with lids</li>
+            <h3>Steps</h3>
+                <li>Measure milk into saucepan (I typically fill the jars then pour the milk from there to the saucepan).</li>
+                <li>Bring the milk to the boil, stirring to prevent it burning on the bottom.  Watch closely to avoid the milk bubbling over.</li>
+                <li>Allow milk to cool until warm, stirring often to prevent skin forming.</li>
+                <li>Stir in the yoghurt.</li>
+                <li>Transfer the hot milk and yoghurt mix to the jars.</li>
+                <li>Keep the jars somewhere warm and draft-free for between 12 and 24 hours.</li>
+                <li>Chill the jars in the fridge overnight to set the yoghurt.</li>
+            <h3>Notes</h3>
+                <li>I normally wrap my jars in an old towel then put them in an insulated shopping bag.</li>
+                <li>Once the yoghurt has set, 2tbsp can be frozen to serve as the starter for the next batch.  Alternatively, the dregs from the jar can be used (remove dregs and wash the jar).</li>
+                <li>If there is no homemade starter available, plain Greek or thickset yoghurt can work instead.</li>
+                <li>Yoghurt made with recipe keeps in the fridge for a couple of weeks.</li>
+                <li>My current strain has been in use since July 2021, saving a bit from each batch to start the next one.</li>
+        </div>
+    )
+}
